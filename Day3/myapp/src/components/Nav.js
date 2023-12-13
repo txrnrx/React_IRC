b363685beb1b6d87147e036b72184dd05a627d7c
txrnrx <tarunrs3tarunrs@gmail.com>
2023-12-13 15:03:41 +0530

    Day3

---
+++ Day3/myapp/src/components/Nav.js
@@ -0,0 +1,14 @@
+
+import Img from "../assets/image/Moon_rotating_full_220px.gif";
+function Nav(){
+    return(
+        <nav className="nav-links">
+        <div className="navbar">
+            <p>
+        <img src={Img} classNmae="srl" alt="img"></img>
+                SPACEC</p>
+        </div>
+        </nav>
+    )
+}
+export default Nav;
---
+++ Day3/myapp/src/components/footer.js
@@ -0,0 +1,12 @@
+function Footer(){
+    return(
+        <div className="footerbar">
+            <marquee>Space is the vast expanse that surrounds our planet and encompasses
+                 everything beyond the Earth's atmosphere. It is a region of immense mystery and wonder, 
+                 filled with stars, planets, galaxies, and other celestial objects. For centuries, humans 
+                 have explored space, driven by curiosity and a desire to understand our place in the universe.</marquee>
+       
+        </div>
+    )
+}
+export default Footer;
